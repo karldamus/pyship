b28993550ae442b7c60ff864e4ef8d961c99f0e2
Karl Damus <44273743+kraftdamus@users.noreply.github.com>
2020-12-19 03:15:18 -0500

    menu_updates

--- game.py
+++ game.py
@@ -4,6 +4,7 @@
 import random
 from rich import print
 from rich.console import Console
+import sys
 
 from data import grid, ai
 from art import art
@@ -11,10 +12,12 @@ from art import art
 console = Console()
 
 # global variables
-size = 0
-ships = 0
-difficulty = 0
+size = 8
+ships = 5
+difficulty = 2
 name = ""
+# unchanged
+min_board_size = 8
 
 
 def main():
@@ -24,13 +27,21 @@ def main():
 def error_message(message):
     console.print("\n  " + str(message), style="bold red")
 
+def confirmation_message(message):
+    console.print("\n  " + str(message), style="bold green")
+
 def welcome():
     art.logo()
     name = input("\n\n To start, please enter your name: ")
     display_menu()
 
 def display_menu():
-    print("\n  [bold green]1.[/bold green] Set [underline]size[/underline] of playable area.")
+    global size
+    global ships
+    global difficulty
+
+    print("\n  [underline]Menu:[/underline]\n")
+    print("  [bold green]1.[/bold green] Set [underline]size[/underline] of playable area.")
     print("  [bold green]2.[/bold green] Set [underline]difficulty[/underline] of enemy.")
     print("  [bold green]3.[/bold green] Set number of [underline]ships[/underline] to play with.")
     print("  [bold green]4.[/bold green] [red]Quit Game.[/red]")
@@ -38,14 +49,40 @@ def display_menu():
     menu_choice = input("\n  Please enter a number (1-4): ")
 
     if menu_choice.isdigit():
-        mc = int(menu_choice)
-        if mc == 1:
-            new_board_size = input("What would you like the size of the board to be? ")
-        
+        settings_changed = False
+        while settings_changed == False:
+            mc = int(menu_choice)
+            # change size of board
+            if mc == 4:
+                terminate_game()
+            if mc == 1:
+                new_board_size = input("\nWhat would you like the size of the board to be? ")
+                if new_board_size.isdigit():
+                    if int(new_board_size) < min_board_size:
+                        error_message("Please enter a number greater than or equal to " + str(min_board_size))
+                    else:
+                        size = int(new_board_size)
+                        confirmation_message("Fantastic! Your new board size is: " + str(size))
+                        display_menu()    
+                else:
+                    error_message("Please enter a valid number!")
+            # change difficulty
+            if mc == 2:
+                new_difficulty = input("\nSet the difficulty by typing 1, 2, or 3: ")
+                if new_difficulty.isdigit():
+                    if int(new_difficulty) < 1 or int(new_difficulty) > 3:
+                        error_message("Please enter a number that is equal to 1, 2, or 3")
+                    else:
+                        difficulty = int(new_difficulty)
+                        confirmation_message("Fantastic! Difficulty has been set to: " + str(difficulty))
+                        display_menu()    
+                else:
+                    error_message("Please enter a valid number!")
     else:
         error_message("Please enter a valid number!")
         display_menu()
 
+'''
 def set_variables(size_01, ships_01, difficulty_01, name_01):
     global size
     global ships
@@ -56,6 +93,7 @@ def set_variables(size_01, ships_01, difficulty_01, name_01):
     ships = ships_01
     difficulty = difficulty_01
     name = name_01
+'''
 
 def setup_game():
     pass
@@ -69,6 +107,8 @@ def fire_missile(coords, list_of_guesses):
 def game_controller():
     pass
 
+def terminate_game():
+    sys.exit()
 
 if __name__ == "__main__":
     main()
